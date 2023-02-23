5fa0758a96e9fcb0865f88b95d3f0cc671208ce2
Pedro <pedro@webglsoft.com>
2023-02-23 22:32:31 +0800

    :heavy_plus_sign: added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          50: "#F5FBEE",
+          100: "#ECF7DE",
+          200: "#D9EFBD",
+          300: "#C8E9A0",
+          400: "#B5E17F",
+          500: "#A2D95E",
+          600: "#85CA30",
+          700: "#649924",
+          800: "#416317",
+          900: "#20320C"
+        }
+        
+      }
+    },
+  },
+  plugins: [],
+}
