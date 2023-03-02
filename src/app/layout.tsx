7e94e668701ef44b14b0e5358f3000acc2fb170f
Pedro Coelho <pedrocoelhodeveloper@gmail.com>
2023-03-02 17:16:37 +0800

    :lipstick: added new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,34 @@
-"use client"
+"use client";
 import "./globals.css";
-import "./reset.css"
+import "./reset.css";
 import Providers from "../providers/Provider";
+import localFont from "next/font/local";
+
+const golosNext = localFont({
+  src: "./GolosText-VariableFont_wght.ttf",
+  display: "swap",
+  variable: "--font-golos",
+});
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={golosNext.variable}>
       <head />
       <Providers>
-        <body className="">
-          <div className="flex flex-col w-full h-full justify-center items-center">
-          {children}
-          </div>
-          </body>
+        <body
+          className="flex flex-col w-full h-full items-center font-golos"
+        >
+          <header className="flex justify-center w-full">
+            <h1 className=" font-black text-6xl my-8 text-primary-1000">Jamboit</h1>
+          </header>
+          <main className="flex flex-col justify-center items-center w-full">
+            {children}
+          </main>
+        </body>
       </Providers>
     </html>
   );
